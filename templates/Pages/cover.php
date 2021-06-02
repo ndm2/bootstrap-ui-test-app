f48b2385dd8c5267c36a5e044ee0641e07ae23f0
ndm2 <ndm2@users.noreply.github.com>
2021-06-02 03:11:18 +0200

    Use the correct nav entries, the parent is not a list element.

--- templates/Pages/cover.php
+++ templates/Pages/cover.php
@@ -6,18 +6,10 @@
 <?php $this->extend('/layout/TwitterBootstrap/cover'); ?>
 
 <?php $this->start('tb_topnav'); ?>
-<li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-</li>
-<li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-</li>
-<li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-</li>
-<li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-</li>
+<a class="nav-link active" aria-current="page" href="#">Active</a>
+<a class="nav-link" href="#">Link</a>
+<a class="nav-link" href="#">Link</a>
+<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
 <?php $this->end(); ?>
 
 <?= $this->Html->image('BootstrapUI.baked-with-cakephp.svg', ['class' => 'mb-4', 'width' => '250']) ?>
