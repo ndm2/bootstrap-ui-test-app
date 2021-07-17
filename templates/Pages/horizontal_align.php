a7eb62cfe931bd63207c3b908ceb6514311d9910
ndm2 <ndm2@users.noreply.github.com>
2021-07-17 15:04:02 +0200

    Adjust horizontal form alignment config.

--- templates/Pages/horizontal_align.php
+++ templates/Pages/horizontal_align.php
@@ -1,17 +1,19 @@
 <?php
 /** @var $this \App\View\AppView */
 
+use BootstrapUI\View\Helper\FormHelper;
+
 $this->layout = 'default';
 
 echo $this->element('controls', ['formOptions' => [
     'align' => [
         'sm' => [
-            'left' => 1,
-            'middle' => 11,
+            FormHelper::GRID_COLUMN_ONE => 1,
+            FormHelper::GRID_COLUMN_TWO => 11,
         ],
         'md' => [
-            'left' => 1,
-            'middle' => 11,
+            FormHelper::GRID_COLUMN_ONE => 1,
+            FormHelper::GRID_COLUMN_TWO => 11,
         ],
     ],
 ]]);
